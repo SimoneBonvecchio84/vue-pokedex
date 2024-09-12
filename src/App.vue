a4f8758cba07f3cab55332576fcca0b4b7f93a59
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-09-12 22:01:20 +0200

    Feat: adds img sprites front and stat skils

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,16 @@ export default {
   },
   data() {
     return {
+      pokemonName: "",
       pokemon: {}
     }
   },
   methods : {
      printPokemon() {
-       axios.get("https://pokeapi.co/api/v2/pokemon/caterpie").then((resp) => {
+       const searchPokemon = `https://pokeapi.co/api/v2/pokemon/${this.pokemonName}`
+       axios.get(searchPokemon).then((resp) => {
         console.log(resp); 
-        this.pokemon = resp;
+        this.pokemon = resp.data;
        });
      }
   }
@@ -23,9 +25,38 @@ export default {
 </script>
 <template>
   <AppHeader/>
-  <button @click="printPokemon">Ottieni Pokemon</button>
-  <div>{{ pokemon.abilities }}</div>
-  <div>
 
+  <input v-model="pokemonName" type="text">
+  <button @click="printPokemon">Ottieni Pokemon</button>
+
+  
+  <div class="d-flex justify-content-center mt-5">
+    <div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <div v-if="(pokemon.sprites.front_default !== null)">
+      <img :src="pokemon.sprites.front_default" alt="">
+    </div>
+    <div v-else>
+        <span>Immagine non presente</span>
+    </div>
+    <li class="list-group-item"> Name:{{ pokemon.name }}</li>
+    <li v-for="(type) in pokemon.types" class="list-group-item">
+      Type: {{ type.type.name }}
+    </li>
+    <li class="list-group-item">
+      Height: {{ pokemon.height }} "
+    </li>
+    <li class="list-group-item">
+      Weight: {{ pokemon.weight }} Ibs
+    </li>
+  </ul>
+   <div>
+     <ul>
+      <li v-for="(curStat) in pokemon.stats" >
+        {{ curStat.stat.name }} {{ curStat.base_stat }}
+      </li>
+     </ul>
+   </div>
+</div>
   </div>
 </template>
