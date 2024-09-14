a25209b33505f30322c4f564afb151234deea647
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-09-14 18:35:16 +0200

    Feat: added btn catch pokemon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,7 +24,8 @@ export default {
       <a class="navbar-brand">Pokemon Hunter</a>
       <div class="d-flex" role="search">
         <input v-model="pokemonName" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button @click="$emit('printPokemon')" class="btn btn-outline-danger" type="submit">Ottieni Pokemon</button>  
+        <button @click="$emit('printPokemon')" class="btn btn-outline-danger" type="submit">Vedi Pokemon</button> 
+        <button @click="$emit('capturePokemon')" class="btn btn-outline-danger" type="submit">Cattura Pokemon</button>    
       </div>
     </div>
   </nav>
